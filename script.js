0d37744b47d8ae11574b6d7c30531cfb006ef8f3
Todd Trulock <toddstrulock@gmail.com>
2023-03-23 18:21:02 -0400

    fixed all the typos and made sure all the buttons and link buttons worked. Need to update business email address.

--- script.js
+++ script.js
@@ -24,3 +24,8 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+function sendEmail() 
+{
+    window.location = "mailto:xyz@yourapplicationdomain.com";
+}
